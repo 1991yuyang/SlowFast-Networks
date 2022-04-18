076f0b6a58ca2af8660606f686c8f53a70b4baf7
yuyang <1230-jj@163.com>
2022-04-18 12:06:20 +0800

    modify predict.py

--- predict.py
+++ predict.py
@@ -55,31 +55,56 @@ def tempral_clip(buffers):
     return buffers
 
 
-def load_data():
+def load_data(video):
+    """
+    param video: 可以填入视频路径，字符串格式；也可以填入帧流，ndarray格式，形状为[D, H, W, C]
+    """
     clips = []
-    cap = cv2.VideoCapture(video_pth)
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    if frame_width < frame_height:
-        new_width = short_side_size
-        resize_ratio = new_width / frame_width
-        new_height = int(resize_ratio * frame_height)
-    else:
-        new_height = short_side_size
-        resize_ratio = new_height / frame_height
-        new_width = int(resize_ratio * frame_width)
-    assert np.all(np.array(crop_size) <= np.array((new_height, new_width))), "crop_size > resized_image_size, (%d,%d) > (%d,%d)" % (crop_size[0], crop_size[1], new_height, new_width)
     buffers = []  # 形状为，[D, H, W, C]
-    for _ in range(frame_count):
-        _, frame = cap.read()
-        if show_video and (cv2.waitKey(35) & 0xff != ord('q')):
-            cv2.imshow("frame", frame)
-        # 1.视频帧转rgb
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # 2.按照短边尺寸进行resize
-        frame = cv2.resize(frame, (new_width, new_height))
-        buffers.append(frame)
+    if isinstance(video, str):
+        # video为video路径
+        cap = cv2.VideoCapture(video)
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        if frame_width < frame_height:
+            new_width = short_side_size
+            resize_ratio = new_width / frame_width
+            new_height = int(resize_ratio * frame_height)
+        else:
+            new_height = short_side_size
+            resize_ratio = new_height / frame_height
+            new_width = int(resize_ratio * frame_width)
+        assert np.all(np.array(crop_size) <= np.array((new_height, new_width))), "crop_size > resized_image_size, (%d,%d) > (%d,%d)" % (crop_size[0], crop_size[1], new_height, new_width)
+        for _ in range(frame_count):
+            _, frame = cap.read()
+            if show_video and (cv2.waitKey(35) & 0xff != ord('q')):
+                cv2.imshow("frame", frame)
+            # 1.视频帧转rgb
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # 2.按照短边尺寸进行resize
+            frame = cv2.resize(frame, (new_width, new_height))
+            buffers.append(frame)
+    if isinstance(video, np.ndarray):
+        # video为视频帧，形状为[D, H, W, C]，D为帧数目，H为高度，W为宽度，C为通道数
+        frame_width = int(video.shape[2])
+        frame_height = int(video.shape[1])
+        if frame_width < frame_height:
+            new_width = short_side_size
+            resize_ratio = new_width / frame_width
+            new_height = int(resize_ratio * frame_height)
+        else:
+            new_height = short_side_size
+            resize_ratio = new_height / frame_height
+            new_width = int(resize_ratio * frame_width)
+        for frame in video:
+            if show_video and (cv2.waitKey(35) & 0xff != ord('q')):
+                cv2.imshow("frame", frame)
+            # 1.视频帧转rgb
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # 2.按照短边尺寸进行resize
+            frame = cv2.resize(frame, (new_width, new_height))
+            buffers.append(frame)
     # 3.归一化
     buffers = np.array(buffers) / 255
     for _ in range(crop_times):
@@ -96,8 +121,8 @@ def load_data():
     return clips
 
 
-def predict():
-    clips = load_data()
+def predict(video_pth):
+    clips = load_data(video_pth)
     softmax_results = []
     predict_loader = make_predict_loader(clips, batch_size, num_workers)
     for d in predict_loader:
@@ -114,6 +139,6 @@ def predict():
 
 
 if __name__ == "__main__":
-    predict_class_name, predict_class_index, confidences = predict()
+    predict_class_name, predict_class_index, confidences = predict(video_pth)
     print("confidence:", confidences)
     print("action name:", predict_class_name)
